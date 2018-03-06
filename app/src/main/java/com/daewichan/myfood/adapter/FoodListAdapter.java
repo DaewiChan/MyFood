9ffe4795bd20d75b960f78ff3304be084f9d795f
DaewiChan <daweichan.chan@gmail.com>
2018-03-06 08:59:25 +0630

    add food image

--- app/src/main/java/com/daewichan/myfood/adapter/FoodListAdapter.java
+++ app/src/main/java/com/daewichan/myfood/adapter/FoodListAdapter.java
@@ -4,8 +4,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.daewichan.myfood.R;
 import com.daewichan.myfood.data.FeatureVO;
 import com.daewichan.myfood.data.request.FeatureRequest;
@@ -55,6 +57,9 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.ItemFo
         @BindView(R.id.tv_name)
         TextView tvName;
 
+        @BindView(R.id.iv_food)
+        ImageView ivFood;
+
         public ItemFoodViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
@@ -62,6 +67,11 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.ItemFo
 
         public void setFood(FeatureVO featureVO){
             tvName.setText(featureVO.getBurppleFeaturedTitle());
+
+            Glide.with(ivFood.getContext())
+                    .load(featureVO.getBurppleFeaturedImage())
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(ivFood);
 
         }
     }
